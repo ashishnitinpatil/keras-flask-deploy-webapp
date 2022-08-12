3bb07c61b307f0127741ac3fa858bf208a9ceccf
Ashish Patil <ashishnitinpatil@gmail.com>
2022-08-13 03:25:05 +0400

    ++pylint dependency and misc linting improvs

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,8 @@
 FROM tensorflow/tensorflow:2.5.1
 
 COPY requirements.txt /
-RUN python3 -m pip install -r /requirements.txt
+RUN python3 -m pip install --upgrade pip
+RUN python3 -m pip install --no-cache-dir -r /requirements.txt
 
 COPY . /app
 WORKDIR /app
--- app.py
+++ app.py
@@ -1,17 +1,13 @@
-import os
-import sys
-
 # Flask
-from flask import Flask, redirect, url_for, request, render_template, Response, jsonify, redirect
-from werkzeug.utils import secure_filename
+from flask import Flask, request, render_template, jsonify
 from gevent.pywsgi import WSGIServer
 
-# TensorFlow and tf.keras
-import tensorflow as tf
-from tensorflow import keras
+from tensorflow.keras.applications.imagenet_utils import (
+    preprocess_input,
+    decode_predictions,
+)
 
-from tensorflow.keras.applications.imagenet_utils import preprocess_input, decode_predictions
-from tensorflow.keras.models import load_model
+# from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 
 # Some utilites
@@ -28,13 +24,14 @@ app = Flask(__name__)
 # or https://www.tensorflow.org/api_docs/python/tf/keras/applications
 
 from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2
-model = MobileNetV2(weights='imagenet')
 
-print('Model loaded. Check http://127.0.0.1:5000/')
+model = MobileNetV2(weights="imagenet")
+
+print("Model loaded. Check http://127.0.0.1:5000/")
 
 
 # Model saved with Keras model.save()
-MODEL_PATH = 'models/your_model.h5'
+MODEL_PATH = "models/your_model.h5"
 
 # Load your own trained model
 # model = load_model(MODEL_PATH)
@@ -52,21 +49,21 @@ def model_predict(img, model):
 
     # Be careful how your trained model deals with the input
     # otherwise, it won't make correct prediction!
-    x = preprocess_input(x, mode='tf')
+    x = preprocess_input(x, mode="tf")
 
     preds = model.predict(x)
     return preds
 
 
-@app.route('/', methods=['GET'])
+@app.route("/", methods=["GET"])
 def index():
     # Main page
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-@app.route('/predict', methods=['GET', 'POST'])
+@app.route("/predict", methods=["GET", "POST"])
 def predict():
-    if request.method == 'POST':
+    if request.method == "POST":
         # Get the image from post request
         img = base64_to_pil(request.json)
 
@@ -77,21 +74,21 @@ def predict():
         preds = model_predict(img, model)
 
         # Process your result for human
-        pred_proba = "{:.3f}".format(np.amax(preds))    # Max probability
-        pred_class = decode_predictions(preds, top=1)   # ImageNet Decode
+        pred_proba = "{:.3f}".format(np.amax(preds))  # Max probability
+        pred_class = decode_predictions(preds, top=1)  # ImageNet Decode
 
-        result = str(pred_class[0][0][1])               # Convert to string
-        result = result.replace('_', ' ').capitalize()
-        
+        result = str(pred_class[0][0][1])  # Convert to string
+        result = result.replace("_", " ").capitalize()
+
         # Serialize the result, you can add additional fields
         return jsonify(result=result, probability=pred_proba)
 
     return None
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # app.run(port=5002, threaded=False)
 
     # Serve the app with gevent
-    http_server = WSGIServer(('0.0.0.0', 5000), app)
+    http_server = WSGIServer(("0.0.0.0", 5000), app)
     http_server.serve_forever()
